76d3c297cad71db3a3f915940c2c507daf4ce6f6
ttnppedr <priest0204@gmail.com>
2022-11-01 09:40:35 +0800

    Use template tag to generator md5 sign

--- app.js
+++ app.js
@@ -1,60 +1,49 @@
 const querystring = require('node:querystring')
 const crypto = require('node:crypto');
 
-module.exports.requestHooks = [
-    context => {
-        const mustSendSign = context.request.getEnvironmentVariable('must_send_md5_sign')
-
-        if (!mustSendSign) {
-            return
+module.exports.templateTags = [{
+    name: 'generateMD5Sign',
+    displayName: 'MD5 sign generator',
+    description: 'Generate md5 sign',
+    args: [
+        {
+            displayName: 'key',
+            type: 'string',
+            placeholder: 'MwIwn0G1G96zZIsZMKpKeJr728kxGHjAvFzQX93IdncleEmhikNGgoqIuS6H'
+        },
+        {
+            displayName: 'excludeParameters',
+            type: 'string',
+            placeholder: 'param1,param2,param3...',
+            defaultValue: 'lang,sign'
+        },
+        {
+            displayName: 'needUrlEncode',
+            type: 'boolean',
+            defaultValue: true
         }
+    ],
+    async run (ctx, key, excludeParameters, needUrlEncode) {
+        // ctx 裡面沒有 request
+        console.log(Object.keys(ctx))
+        console.log(Object.keys(ctx.context))
+        console.log(Object.keys(ctx.app))
+        // const requestBody = context.request.getBody()
+        // let parameters = {}
 
-        const secretKey = context.request.getEnvironmentVariable('md5_sign_secret_key')
-        const requestBody = context.request.getBody()
-        let parameters = {}
+        // if (requestBody.mimeType === 'application/json') {
+        //     parameters = JSON.parse(requestBody.text)
+        // }
+        // console.log(excludeParameters.split(','))
 
-        if (requestBody.mimeType === 'application/json') {
-            parameters = JSON.parse(requestBody.text)
-        }
+        // if (context.request.getMethod().toUpperCase() === 'GET') {
+        //     parameters = context.request.getParameters().reduce((accumulator, parameter) => {
+        //         accumulator[parameter['name']] = parameter['value']
+        //
+        //         return accumulator
+        //     }, {})
+        // }
 
-        if (context.request.getMethod().toUpperCase() === 'GET') {
-            parameters = context.request.getParameters().reduce((accumulator, parameter) => {
-                accumulator[parameter['name']] = parameter['value']
-
-                return accumulator
-            }, {})
-        }
-
-        delete parameters['sign']
-        delete parameters['lang']
-
-        const orderedParameters = Object.keys(parameters).sort().reduce(
-            (ordered, key) => {
-                ordered[key] = parameters[key]
-
-                return ordered
-            }, {}
-        )
-
-        const mustUrlEncode = context.request.getEnvironmentVariable('md5_sign_must_url_encode')
-        let preSignString = querystring.encode(orderedParameters)
-
-        if (!mustUrlEncode) {
-            preSignString = querystring.unescape(preSignString)
-        }
-
-        parameters['sign'] = crypto.createHash('md5').update(
-            preSignString + `&secret_key=${secretKey}`,
-            'utf8'
-        ).digest('hex')
-
-        if (context.request.getMethod().toUpperCase() === 'GET') {
-            context.request.addParameter('sign', parameters['sign'])
-        } else {
-            context.request.setBody({
-                mimeType: 'application/json',
-                text: JSON.stringify(parameters),
-            })
-        }
+        return '123'
     }
-]
+}];
